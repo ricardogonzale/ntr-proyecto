271b3eb24a870e7eb4b888f6afbf1e2773acdb73
Ricardo Gonzalez <ricardogonzale@gmail.com>
2022-02-12 21:57:22 -0400

    Generando proyecto y compatibilidad con Vue y Vuetify

--- public/js/app.js
+++ public/js/app.js
@@ -52971,8 +52971,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vuex__WEBPACK_IMPORTED_MODULE_1__
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\bookExchange\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\bookExchange\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\ntr-proyecto\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\ntr-proyecto\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
